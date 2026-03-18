7362e7f4049bed9d25430443b52d2764c6294e4b
abinaya-s-4 <abinaya.selva2020@gmail.com>
2026-03-18 12:16:37 +0530

    Started amazon.js

---
+++ amazon.js
@@ -0,0 +1,26 @@
+const products = [{
+  name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+  img: 'images/ratings/rating-45.png',
+  ratings: {
+    stars: 4.5,
+    count: 87,
+  },
+  price : 849,
+},{
+  name : 'Intermediate Size Basketball',
+  img : 'images/ratings/rating-40.png',
+  ratings :{
+    stars:4,
+    count: 127,
+  },
+  price : 499,
+
+},{
+  name : 'Adults Plain Cotton T-Shirt - 2 Pack',
+  img : 'images/ratings/rating-45.png',
+  ratings :{
+    stars:4.5 ,
+    count: 56,
+  },
+  price : 1799,
+}]
