87192a99ba66a283fa7fedb90676f38042a4242e
abinaya-s-4 <abinaya.selva2020@gmail.com>
2026-03-27 14:51:00 +0530

    saved data and generated html for paymentSummary

--- checkout/paymentSummary.js
+++ checkout/paymentSummary.js
@@ -1,11 +1,56 @@
 import {cart} from '../../data/cart.js'
 import { getProduct } from '../../data/products.js';
+import { getDeliveryOption } from '../data/deliveryOptions.js';
 
 export function renderPaymentSummary(){
-  let productprice = 0;
+  let productPrice = 0;
+  let shippingPrice =0;
   cart.forEach((cartItem) =>{
     const product = getProduct(cartItem.productId);
-    productprice += product.price * cartItem.quantity;
+    productPrice += product.price * cartItem.quantity;
+
+    const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
+    shippingPrice += deliveryOption.price;
     
   });
+  const totalBeforeTax = productPrice + shippingPrice;
+  const taxPrice = totalBeforeTax * 0.1;
+  const totalPrice = taxPrice + totalBeforeTax;
+
+  const paymentSummaryHtML = `
+    <div class="payment-summary-title">
+      Order Summary
+    </div>
+
+    <div class="payment-summary-row">
+      <div>Items (3):</div>
+      <div class="payment-summary-money">₹${productPrice}</div>
+    </div>
+
+    <div class="payment-summary-row">
+      <div>Shipping &amp; handling:</div>
+      <div class="payment-summary-money">₹${shippingPrice}</div>
+    </div>
+
+    <div class="payment-summary-row subtotal-row">
+      <div>Total before tax:</div>
+      <div class="payment-summary-money">₹${totalBeforeTax}</div>
+    </div>
+
+    <div class="payment-summary-row">
+      <div>Estimated tax (10%):</div>
+      <div class="payment-summary-money">₹${taxPrice}</div>
+    </div>
+
+    <div class="payment-summary-row total-row">
+      <div>Order total:</div>
+      <div class="payment-summary-money">₹${totalPrice}</div>
+    </div>
+
+    <button class="place-order-button button-primary">
+      Place your order
+    </button>
+  `;
+  document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHtML;
+
 }
